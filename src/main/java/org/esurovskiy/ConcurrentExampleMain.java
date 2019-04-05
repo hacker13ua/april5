f358bc6f3499e99c1df4b36c0fb7e5fa2f967648
esurovskiy <surovskiy@trafficdna.com>
2019-04-05 21:27:34 +0300

    1

--- src/main/java/org/esurovskiy/ConcurrentExampleMain.java
+++ src/main/java/org/esurovskiy/ConcurrentExampleMain.java
@@ -1,15 +1,20 @@
 package org.esurovskiy;
 
 public class ConcurrentExampleMain {
-    private static int x = 0;
+    private volatile static int x = 0;
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
-                x++;
-            }
-        });
-        thread.start();
+    public static void main(String[] args) throws InterruptedException {
+        for (int j = 0; j < 10; j++) {
+            Thread thread = new Thread(() -> {
+                System.out.println("Thread started");
+                for (int i = 0; i < 1000000; i++) {
+                    x++;
+                }
+            });
+            thread.start();
+        }
+        System.out.println("Start sleep");
+        Thread.sleep(100);
         System.out.println(x);
     }
 }
